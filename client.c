0c88129054f10bf5e5e14bf35229ea8d130d5f45
jkajs <jajsovajulia@gmail.com>
2024-01-07 23:32:10 +0100

    julia

--- client.c
+++ client.c
@@ -1,59 +1,97 @@
-#include <unistd.h>     
-#include <stdio.h>      
-#include <string.h>     
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
-#include <sys/un.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "pexeso.h"
 
 void handler( int signalNumber)
 {
-	exit( 0 );
+    printf( "Game has ended.\n" );
+    exit( 0 );
+}
+
+bool is_valid( char *buffer )
+{
+    for ( int i = 0; i < 32 && buffer[ i ] != 0; ++i )
+    {
+        if ( buffer[ i ] != '\n' && ( buffer[ i ] < '0' || buffer[ i ] > '9' ) )
+        {
+            printf( "Invalid sign. Repeate.\n" );
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void load_and_check( int *write_to )
+{
+    while ( true )
+    {
+        char buffer[ 32 ] = { 0 };
+        scanf( "%s", buffer );
+
+        if ( !is_valid( buffer ) )
+            continue;
+
+        *write_to = atoi( buffer );
+        return;
+    }
 }
 
 void player( int fd )
 {
-	while ( true )
-	{
-		position pos;
-		printf( "Put coordinations of first tile: \n" );
-		scanf( " %d %d", &pos.x1, &pos.y1 );
+    while ( true )
+    {
+        position pos;
 
-		printf( "Put coordinations of second tile: \n" );
-		scanf( " %d %d", &pos.x2, &pos.y2 );
+        printf( "Put x coordinate of first tile: \n" );
+        load_and_check( &pos.x1 );
 
-		if ( write( fd, &pos, sizeof( position ) ) != sizeof( position ) )
-		{
-			perror( "write" );
-			exit( -1 );
-		}
-	}
+        printf( "Put y coordinate of first tile: \n" );
+        load_and_check( &pos.y1 );
+
+        printf( "Put x coordinate of second tile: \n" );
+        load_and_check( &pos.x2 );
+
+        printf( "Put y coordinate of second tile: \n" );
+        load_and_check( &pos.y2 );
+
+        if ( write( fd, &pos, sizeof( position ) ) != sizeof( position ) )
+        {
+            perror( "write" );
+            exit( -1 );
+        }
+    }
 }
 
-int main()
-{
-	if ( signal( SIGPIPE, handler ) == SIG_ERR )
-	{
-		perror( "signal" );
-		exit( -1 );
-	}
+int main() {
+    if (signal(SIGPIPE, handler) == SIG_ERR) {
+        perror("signal");
+        return -1;
+    }
 
-	int sock_fd = socket( AF_UNIX, SOCK_STREAM, 0 );
-	if ( sock_fd == -1 )
-	{
-		perror( "socket" );
-		exit( -1 );
-	}
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd == -1) {
+        perror("socket");
+        return -1;
+    }
 
-	struct sockaddr_un sa = { .sun_family = AF_UNIX, .sun_path = "game" };
+    struct sockaddr_in sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(12345);
+    sa.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	if ( connect( sock_fd, ( struct sockaddr * ) &sa, sizeof( sa ) ) == -1 )
-	{
-		perror( "connect" );
-		exit( -1 );
-	}
+    if (connect(sock_fd, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
+        perror("connect");
+        return -1;
+    }
 
-	player( sock_fd );
-	exit( 0 );
+    player(sock_fd);
+    return 0;
 }
